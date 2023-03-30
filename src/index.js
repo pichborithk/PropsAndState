5f285d6554ca87db2001961b486cbe9a75de52fd
Pichborith Kong <pichborith@gmail.com>
2023-03-29 19:23:45 -0700

    finish workshop

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 // write your Color component here
 
-const Picker = () => {
+const Color = (props) => {
+  const classColor =
+    props.color === props.selectedColor
+      ? props.color + ' selected'
+      : props.color;
+
   return (
-    <div id="container">
-      <div id="navbar">
+    <div
+      className={classColor}
+      onClick={() => props.selectColor(props.color)}
+    ></div>
+  );
+};
+
+const Picker = () => {
+  const [selectedColor, setSelectedColor] = useState('');
+
+  const selectColor = (colorName) => {
+    setSelectedColor(colorName);
+  };
+
+  return (
+    <div id='container'>
+      <div id='navbar'>
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id='colors-list'>
+        <Color
+          color='red'
+          selectColor={selectColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color='blue'
+          selectColor={selectColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color='green'
+          selectColor={selectColor}
+          selectedColor={selectedColor}
+        />
+      </div>
     </div>
   );
-}
+};
 
-ReactDOM.render(
-  <Picker />,
-  document.getElementById('app')
-);
+ReactDOM.render(<Picker />, document.getElementById('app'));
